11ea56994b73e87a062c9d8732828e9ed6a3b3a9
Luigi Foscari <luigi.foscari@icloud.com>
2020-08-20 14:50:20 +0200

    started working on the dashborad

---
+++ dashboard/src/App.vue
@@ -0,0 +1,59 @@
+<template>
+  <div id="app" class="container my-5">
+    <div class="row">
+      <ContainerSlot>
+        <a class="nes-badge mr-3 is-splited">
+          <span class="is-dark">gear</span>
+          <span class="is-primary">
+            5
+            <!-- <sup>th</sup> -->
+          </span>
+        </a>
+
+        <a class="nes-badge mr-3">
+          <span class="is-success">Lights: on</span>
+        </a>
+      </ContainerSlot>
+
+      <ContainerSlot>
+        <p>Throttle position: 25%</p>
+        <progress class="nes-progress" value="25" max="100"></progress>
+      </ContainerSlot>
+
+      <ContainerSlot>
+        <p>Oil temperature: 85°</p>
+        <progress class="nes-progress is-error" value="85" max="100"></progress>
+      </ContainerSlot>
+
+      <ContainerSlot>
+        <p>RPM: 2000</p>
+        <progress class="nes-progress is-success" value="2000" max="5000"></progress>
+      </ContainerSlot>
+
+      <ContainerSlot>
+        <p>RPM: 3000</p>
+        <progress class="nes-progress is-warning" value="3000" max="5000"></progress>
+      </ContainerSlot>
+
+      <ContainerSlot>
+        <p>RPM: 4500</p>
+        <progress class="nes-progress is-error" value="4500" max="5000"></progress>
+      </ContainerSlot>
+    </div>
+  </div>
+</template>
+
+<script>
+import ContainerSlot from "./components/ContainerSlot.vue";
+
+export default {
+  name: "App",
+  components: {
+    ContainerSlot,
+  },
+};
+</script>
+
+<style lang="scss">
+@import url("assets/bootstrap-grid.min.css");
+</style>
---
+++ dashboard/src/components/ContainerSlot.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="col-12 col-md-4 mb-3">
+    <!-- <div class="nes-container is-rounded"> -->
+    <slot></slot>
+    <!-- </div> -->
+  </div>
+</template>
