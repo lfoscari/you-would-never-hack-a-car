98ca718705ead3e8cd797ecadcaefa310e18f35a
Luigi Foscari <luigi.foscari@icloud.com>
2020-08-27 14:20:42 +0200

    refactoring, change build parameters for a slimmer build

--- dashboard/src/App.vue
+++ dashboard/src/App.vue
@@ -4,100 +4,95 @@
       <h1>Error reading the data file</h1>
     </div>
     <div v-else>
-      <div v-if="loading">
-        <h1>Loading...</h1>
-      </div>
-      <div v-else>
-        <div class="row justify-content-center">
-          <div v-if="typeof engine.gear != 'undefined'" class="col-sm-3 col-6 gy-3">
-            <badge name="current gear" unit="Gear" :value="engine.gear"></badge>
-          </div>
-
-          <div v-if="typeof engine.lights != 'undefined'" class="col-sm-3 col-6 gy-3">
-            <badge name="lights status" unit="Lights" :value="engine.lights ? 'on' : 'off'"></badge>
-          </div>
-
-          <div v-if="typeof engine.air_temperature != 'undefined'" class="col-sm-3 col-6 gy-3">
-            <badge name="current temperature" unit="C°" :value="engine.air_temperature"></badge>
-          </div>
-
-          <div v-if="typeof engine.fuel != 'undefined'" class="col-sm-3 col-6 gy-3">
-            <badge name="current fuel level" unit="Fuel" :value="engine.fuel"></badge>
-          </div>
+      <div class="row justify-content-center">
+        <div v-if="typeof engine.gear != 'undefined'" class="col-sm-3 col-6 gy-3">
+          <badge name="current gear" unit="Gear" :value="engine.gear"></badge>
         </div>
 
-        <div class="row justify-content-center">
-          <div v-if="typeof engine.throttle != 'undefined'" class="col-sm-4 col-12 gy-3">
-            <progress-bar
-              name="Throttle position"
-              unit="%"
-              :level="engine.throttle"
-              :min="0"
-              :max="100"
-            ></progress-bar>
-          </div>
+        <div v-if="typeof engine.lights != 'undefined'" class="col-sm-3 col-6 gy-3">
+          <badge name="lights status" unit="Lights" :value="engine.lights ? 'on' : 'off'"></badge>
+        </div>
 
-          <div
-            v-if="typeof engine.oil_temp != 'undefined' && typeof engine.critical_oil_temp != 'undefined'"
-            class="col-sm-4 col-12 gy-3"
-          >
-            <progress-bar
-              name="Oil temperature"
-              unit="C°"
-              :level="engine.oil_temp"
-              :min="0"
-              :max="engine.critical_oil_temp"
-              is_graded="true"
-            ></progress-bar>
-          </div>
+        <div v-if="typeof engine.air_temperature != 'undefined'" class="col-sm-3 col-6 gy-3">
+          <badge name="current temperature" unit="C°" :value="engine.air_temperature"></badge>
+        </div>
 
-          <div
-            v-if="typeof engine.rpm != 'undefined' && typeof engine.max_rpm != 'undefined'"
-            class="col-sm-4 col-12 gy-3"
-          >
-            <progress-bar
-              name="RPM"
-              unit
-              :level="engine.rpm"
-              :min="0"
-              :max="engine.max_rpm"
-              is_graded="true"
-            ></progress-bar>
-          </div>
+        <div v-if="typeof engine.fuel != 'undefined'" class="col-sm-3 col-6 gy-3">
+          <badge name="current fuel level" unit="Fuel" :value="engine.fuel"></badge>
+        </div>
+      </div>
 
-          <div v-if="typeof engine.throttle != 'undefined'" class="col-sm-4 col-12 gy-3">
-            <progress-bar
-              name="Throttle position"
-              unit="%"
-              :level="engine.throttle"
-              :min="0"
-              :max="100"
-            ></progress-bar>
-          </div>
+      <div class="row justify-content-center">
+        <div v-if="typeof engine.throttle != 'undefined'" class="col-sm-4 col-12 gy-3">
+          <progress-bar
+            name="Throttle position"
+            unit="%"
+            :level="engine.throttle"
+            :min="0"
+            :max="100"
+          ></progress-bar>
+        </div>
 
-          <div v-if="typeof engine.throttle != 'undefined'" class="col-sm-4 col-12 gy-3">
-            <progress-bar
-              name="Throttle position"
-              unit="%"
-              :level="engine.throttle"
-              :min="0"
-              :max="100"
-            ></progress-bar>
-          </div>
+        <div
+          v-if="typeof engine.oil_temp != 'undefined' && typeof engine.critical_oil_temp != 'undefined'"
+          class="col-sm-4 col-12 gy-3"
+        >
+          <progress-bar
+            name="Oil temperature"
+            unit="C°"
+            :level="engine.oil_temp"
+            :min="0"
+            :max="engine.critical_oil_temp"
+            is_graded="true"
+          ></progress-bar>
+        </div>
 
-          <div v-if="typeof engine.throttle != 'undefined'" class="col-sm-4 col-12 gy-3">
-            <progress-bar
-              name="Throttle position"
-              unit="%"
-              :level="engine.throttle"
-              :min="0"
-              :max="100"
-            ></progress-bar>
-          </div>
+        <div
+          v-if="typeof engine.rpm != 'undefined' && typeof engine.max_rpm != 'undefined'"
+          class="col-sm-4 col-12 gy-3"
+        >
+          <progress-bar
+            name="RPM"
+            unit
+            :level="engine.rpm"
+            :min="0"
+            :max="engine.max_rpm"
+            is_graded="true"
+          ></progress-bar>
+        </div>
 
-          <div class="col-12 gy-3">
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit atque fugiat mollitia iste perferendis doloremque quos nihil ratione quasi aperiam provident a, labore inventore vel amet minus ea sapiente ab.</p>
-          </div>
+        <div v-if="typeof engine.throttle != 'undefined'" class="col-sm-4 col-12 gy-3">
+          <progress-bar
+            name="Throttle position"
+            unit="%"
+            :level="engine.throttle"
+            :min="0"
+            :max="100"
+          ></progress-bar>
+        </div>
+
+        <div v-if="typeof engine.throttle != 'undefined'" class="col-sm-4 col-12 gy-3">
+          <progress-bar
+            name="Throttle position"
+            unit="%"
+            :level="engine.throttle"
+            :min="0"
+            :max="100"
+          ></progress-bar>
+        </div>
+
+        <div v-if="typeof engine.throttle != 'undefined'" class="col-sm-4 col-12 gy-3">
+          <progress-bar
+            name="Throttle position"
+            unit="%"
+            :level="engine.throttle"
+            :min="0"
+            :max="100"
+          ></progress-bar>
+        </div>
+
+        <div class="col-12 gy-3">
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit atque fugiat mollitia iste perferendis doloremque quos nihil ratione quasi aperiam provident a, labore inventore vel amet minus ea sapiente ab.</p>
         </div>
       </div>
     </div>
@@ -105,27 +100,29 @@
 </template>
 
 <script>
-// import axios from "axios";
-
-// import ContainerSlot from "./components/ContainerSlot.vue";
 import ProgressBar from "./components/ProgressBar.vue";
 import Badge from "./components/Badge.vue";
-
-// let ENGINE_DATA = "./data.json";
-// let REFRESH_TIMER = 1000;
 
 export default {
   name: "App",
   components: {
-    // ContainerSlot,
     ProgressBar,
     Badge,
   },
   data() {
     return {
-      loading: true,
       error: false,
-      engine: null,
+      engine: {
+        gear: 0,
+        lights: false,
+        throttle: 0,
+        oil_temp: 0,
+        critical_oil_temp: 100,
+        rpm: 0,
+        max_rpm: 100,
+        air_temperature: 0,
+        fuel: 0,
+      },
     };
   },
   mounted() {
@@ -135,7 +132,6 @@ export default {
       source.addEventListener(
         "open",
         function () {
-          this.loading = false;
           console.log("Events Connected");
         },
         false
@@ -147,6 +143,8 @@ export default {
           if (e.target.readyState != EventSource.OPEN) {
             console.log("Events Disconnected");
             this.error = true;
+
+            // Tenta di riconnetterti, altirmenti ricarica la pagina
           }
         },
         false
@@ -154,28 +152,14 @@ export default {
 
       source.addEventListener(
         "dataupdate",
-        function (e) {
-          console.log("new Data", e.data);
-          console.log(JSON.parse(e.data));
-          this.engine = JSON.parse(e.data);
-        },
+        (e) => this.updateEngine(e.data),
         false
       );
     }
-    // setInterval(() => {
-    //   this.update();
-    // }, REFRESH_TIMER);
   },
   methods: {
-    update() {
-      // axios
-      //   .get(ENGINE_DATA)
-      //   .then((response) => (this.engine = response.data))
-      //   .catch((error) => {
-      //     console.log(error);
-      //     this.error = true;
-      //   })
-      //   .finally(() => (this.loading = false));
+    updateEngine(data) {
+      this.engine = JSON.parse(data);
     },
   },
 };
